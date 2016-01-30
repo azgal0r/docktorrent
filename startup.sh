99afc16e746a1c674a16ad5b43439abbf7761f43
Azgal0r <you@example.com>
2016-01-30 12:38:58 +0100

    Modified startup

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-docker run -dt -p 80:80 -p 45566:45566 -p 9527:9527/udp   --dns 8.8.8.8  -v /home/cloudsto/rtorrent/:/rtorrent -v /media/data/:/media_data docktorrent
+docker run -dt -p 9000:80 -p 45566:45566 -p 9527:9527/udp  --net=htpc_network --name=rtorrentcontainer --dns 8.8.8.8  -v /home/cloudsto/rtorrent/:/rtorrent -v /media/data/:/media_data docktorrent
