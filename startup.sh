d4c96d39e2fef06217f34555a242a2bab93d31ab
azgal0r <dindonsauvage@gmail.com>
2016-02-06 14:00:02 +0100

    Startup script to /opt and upstart service

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-docker run -dt -p 9000:80 -p 45566:45566 -p 9527:9527/udp  --net=htpc_network --name=rtorrentcontainer --dns 8.8.8.8  -v /home/cloudsto/rtorrent/:/rtorrent -v /media/data/:/media_data docktorrent docktorrent
+docker run -dt -p 9000:80 -p 45566:45566 -p 9527:9527/udp  --net=htpc_network --name=rtorrentcontainer --dns 8.8.8.8  -v /media/data/config/rtorrent:/rtorrent -v /media/data/:/media_data docktorrent docktorrent
